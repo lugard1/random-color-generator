0091584e2bf161189c84db56635c71d1ba42617a
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2025-01-25 10:49:17 +0100

    Update

---
+++ index.js
@@ -0,0 +1,35 @@
+const containerEl = document.querySelector('.container');
+
+let array = 30;
+
+for (let index = 0; index < array; index++) {
+  const colorContainerEl = document.createElement('div');
+  colorContainerEl.classList.add('color-container');
+  containerEl.appendChild(colorContainerEl)
+}
+
+const colorContainerEls = document.querySelectorAll('.color-container')
+
+generateColors();
+
+function generateColors(){
+  colorContainerEls.forEach((colorContainerEl)=>{
+    const newColorCode = randomColor()
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode;
+  })
+
+}
+
+function randomColor(){
+  const chars = '0123456789abcdef';
+  const colorCodeLength = 6;
+  let colorCode = ""
+
+  for (let index = 0; index < colorCodeLength; index++) {
+    
+    const randomNum =Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode
+}
